0e6f7e45bb56a52b205f199496ee316587070cb8
JNIlluvium <122263283+JNIlluvium@users.noreply.github.com>
2023-12-21 09:55:03 +0100

    cleanup

--- src/App.cpp
+++ src/App.cpp
@@ -7,17 +7,17 @@
 #include <cmath>
 
 App::App() {
-    window = new sf::RenderWindow(sf::VideoMode(screenWidth, screenHeight), "Raycaster");
+    window = new sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Raycaster");
     window->setVerticalSyncEnabled(true);
 
     buffer = new sf::Image();
-    buffer->create(screenWidth, screenHeight, sf::Color::Black);
+    buffer->create(RENDER_WIDTH, RENDER_HEIGHT, sf::Color::Black);
 
     texture = new sf::Texture();
     texture->loadFromImage(*buffer);
 
-    bufferSprite = new sf::Sprite();
-    bufferSprite->setTexture(*texture, true);
+    bufferRect = new sf::RectangleShape(sf::Vector2f(WINDOW_WIDTH, WINDOW_HEIGHT));
+    bufferRect->setTexture(texture, true);
 }
 
 App::~App() {
@@ -25,7 +25,7 @@ App::~App() {
         free(image);
     }
     textures.clear();
-    free(bufferSprite);
+    free(bufferRect);
     free(texture);
     free(buffer);
     free(window);
@@ -40,14 +40,14 @@ int App::run() {
     textures.push_back(new sf::Image());
     textures.push_back(new sf::Image());
     textures.push_back(new sf::Image());
-    textures[0]->loadFromFile("textures/eagle.png");
-    textures[1]->loadFromFile("textures/redbrick.png");
-    textures[2]->loadFromFile("textures/purplestone.png");
-    textures[3]->loadFromFile("textures/greystone.png");
-    textures[4]->loadFromFile("textures/bluestone.png");
-    textures[5]->loadFromFile("textures/mossy.png");
-    textures[6]->loadFromFile("textures/wood.png");
-    textures[7]->loadFromFile("textures/colorstone.png");
+    textures[0]->loadFromFile("textures/wool_colored_yellow.png");
+    textures[1]->loadFromFile("textures/wool_colored_silver.png");
+    textures[2]->loadFromFile("textures/wool_colored_orange.png");
+    textures[3]->loadFromFile("textures/wool_colored_black.png");
+    textures[4]->loadFromFile("textures/wool_colored_blue.png");
+    textures[5]->loadFromFile("textures/wool_colored_brown.png");
+    textures[6]->loadFromFile("textures/wool_colored_gray.png");
+    textures[7]->loadFromFile("textures/wool_colored_red.png");
 
     while (window->isOpen()) {
         handleInput();
@@ -67,15 +67,15 @@ void App::handleInput() {
     const double rotSpeed = frameTime * 3.0; //the constant value is in radians/second
 
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::W)) {
-        if (worldMap[static_cast<int>(posX + dirX * moveSpeed * wallDistance)][static_cast<int>(posY)] == false)
+        if (worldMap[static_cast<int>(posX + dirX * moveSpeed * WALL_DISTANCE)][static_cast<int>(posY)] == false)
             posX += dirX * moveSpeed;
-        if (worldMap[static_cast<int>(posX)][static_cast<int>(posY + dirY * moveSpeed * wallDistance)] == false)
+        if (worldMap[static_cast<int>(posX)][static_cast<int>(posY + dirY * moveSpeed * WALL_DISTANCE)] == false)
             posY += dirY * moveSpeed;
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::S)) {
-        if (worldMap[static_cast<int>(posX - dirX * moveSpeed * wallDistance)][static_cast<int>(posY)] == false)
+        if (worldMap[static_cast<int>(posX - dirX * moveSpeed * WALL_DISTANCE)][static_cast<int>(posY)] == false)
             posX -= dirX * moveSpeed;
-        if (worldMap[static_cast<int>(posX)][static_cast<int>(posY - dirY * moveSpeed * wallDistance)] == false)
+        if (worldMap[static_cast<int>(posX)][static_cast<int>(posY - dirY * moveSpeed * WALL_DISTANCE)] == false)
             posY -= dirY * moveSpeed;
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Scancode::D)) {
@@ -105,108 +105,107 @@ void App::handleInput() {
 }
 
 void App::clearBuffer(const int x) const {
-    for (int y = 0; y < screenHeight; y++) {
+    for (int y = 0; y < RENDER_HEIGHT; y++) {
         buffer->setPixel(x, y, sf::Color::Black);
     }
 }
 
-void App::calculateStep(const double &rayDirX, const double &rayDirY, const int &mapX, const int &mapY,
-                        double &sideDistX, double &sideDistY, const double deltaDistX, const double deltaDistY,
-                        int &stepX, int &stepY) const {
+void App::calculateStep(const sf::Vector2<double> &rayDir, const sf::Vector2<double> &deltaDist,
+                        const sf::Vector2i map, sf::Vector2<double> &sideDist,
+                        sf::Vector2i &step) const {
     //what direction to step in x or y-direction (either +1 or -1)
-    if (rayDirX < 0) {
-        stepX = -1;
-        sideDistX = (posX - mapX) * deltaDistX;
+    if (rayDir.x < 0) {
+        step.x = -1;
+        sideDist.x = (posX - map.x) * deltaDist.x;
     } else {
-        stepX = 1;
-        sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+        step.x = 1;
+        sideDist.x = (map.x + 1.0 - posX) * deltaDist.x;
     }
-    if (rayDirY < 0) {
-        stepY = -1;
-        sideDistY = (posY - mapY) * deltaDistY;
+    if (rayDir.y < 0) {
+        step.y = -1;
+        sideDist.y = (posY - map.y) * deltaDist.y;
     } else {
-        stepY = 1;
-        sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+        step.y = 1;
+        sideDist.y = (map.y + 1.0 - posY) * deltaDist.y;
     }
 }
 
-void App::performDDA(int &mapX, int &mapY, int &side, double &sideDistX, double &sideDistY, const double deltaDistX,
-                     const double deltaDistY, const int stepX, const int stepY) {
+void App::performDDA(int &side, const sf::Vector2<double> &deltaDist, const sf::Vector2i &step, sf::Vector2i map,
+                     sf::Vector2<double> &sideDist) {
     int hit = 0; //was there a wall hit?
     while (hit == 0) {
         //jump to next map square, either in x-direction, or in y-direction
-        if (sideDistX < sideDistY) {
-            sideDistX += deltaDistX;
-            mapX += stepX;
+        if (sideDist.x < sideDist.y) {
+            sideDist.x += deltaDist.x;
+            map.x += step.x;
             side = 0;
         } else {
-            sideDistY += deltaDistY;
-            mapY += stepY;
+            sideDist.y += deltaDist.y;
+            map.y += step.y;
             side = 1;
         }
         //Check if ray has hit a wall
-        if (worldMap[mapX][mapY] > 0) hit = 1;
+        if (worldMap[map.x][map.y] > 0) hit = 1;
     }
 }
 
-void App::calculate(const int x, double &rayDirX, double &rayDirY, int &mapX, int &mapY, double &perpWallDist,
+void App::calculate(const int x, sf::Vector2<double> &rayDir, sf::Vector2i map, double &perpWallDist,
                     int &side,
                     int &lineHeight, int &drawStart, int &drawEnd) const {
     //calculate ray position and direction
-    const double cameraX = 2 * x / static_cast<double>(screenWidth) - 1; //x-coordinate in camera space
-    rayDirX = dirX + planeX * cameraX;
-    rayDirY = dirY + planeY * cameraX;
-    mapX = static_cast<int>(posX);
-    mapY = static_cast<int>(posY);
+    const double cameraX = 2 * x / static_cast<double>(RENDER_WIDTH) - 1; //x-coordinate in camera space
+    rayDir = sf::Vector2(dirX + planeX * cameraX, dirY + planeY * cameraX);
+    map = sf::Vector2i(static_cast<int>(posX), static_cast<int>(posY));
 
     //length of ray from current position to next x or y-side
-    double sideDistX, sideDistY;
-    int stepX, stepY;
+    sf::Vector2<double> sideDist;
+    sf::Vector2i step;
 
     //length of ray from one x or y-side to next x or y-side
-    double deltaDistX = (rayDirX == 0) ? 1e30 : std::abs(1 / rayDirX);
-    double deltaDistY = (rayDirY == 0) ? 1e30 : std::abs(1 / rayDirY);
+    const sf::Vector2<double> deltaDist = sf::Vector2<double>(
+        (rayDir.x == 0) ? 1e30 : std::abs(1 / rayDir.x),
+        (rayDir.y == 0) ? 1e30 : std::abs(1 / rayDir.y));
 
     //calculate step and initial sideDist
-    calculateStep(rayDirX, rayDirY, mapX, mapY, sideDistX, sideDistY, deltaDistX, deltaDistY, stepX, stepY);
+    calculateStep(rayDir, deltaDist, map, sideDist, step);
 
     //perform DDA
-    performDDA(mapX, mapY, side, sideDistX, sideDistY, deltaDistX, deltaDistY, stepX, stepY);
+    performDDA(side, deltaDist, step, map, sideDist);
 
     //Calculate distance projected on camera direction (Euclidean distance would give fisheye effect!)
-    if (side == 0) perpWallDist = (sideDistX - deltaDistX);
-    else perpWallDist = (sideDistY - deltaDistY);
-    lineHeight = static_cast<int>(screenHeight / perpWallDist);
+    if (side == 0) perpWallDist = (sideDist.x - deltaDist.x);
+    else perpWallDist = (sideDist.y - deltaDist.y);
+    lineHeight = static_cast<int>(RENDER_HEIGHT / perpWallDist);
 
-    drawStart = -lineHeight / 2 + screenHeight / 2;
+    drawStart = -lineHeight / 2 + RENDER_HEIGHT / 2;
     if (drawStart < 0) drawStart = 0;
-    drawEnd = lineHeight / 2 + screenHeight / 2;
-    if (drawEnd >= screenHeight) drawEnd = screenHeight - 1;
+    drawEnd = lineHeight / 2 + RENDER_HEIGHT / 2;
+    if (drawEnd >= RENDER_HEIGHT) drawEnd = RENDER_HEIGHT - 1;
 }
 
-void App::getTextureParameters(const double rayDirX, const double rayDirY, const int mapX, const int mapY,
+void App::getTextureParameters(const sf::Vector2<double> &rayDir, const sf::Vector2i map,
                                const double perpWallDist, const int side, const int lineHeight, const int drawStart,
                                int &texNum, int &texX, double &step, double &texPos) const {
-    texNum = worldMap[mapX][mapY] - 1;
+    texNum = worldMap[map.x][map.y] - 1;
 
     //calculate value of wallX
     double wallX; //where exactly the wall was hit
-    if (side == 0) wallX = posY + perpWallDist * rayDirY;
-    else wallX = posX + perpWallDist * rayDirX;
+    if (side == 0) wallX = posY + perpWallDist * rayDir.y;
+    else wallX = posX + perpWallDist * rayDir.x;
     wallX -= floor((wallX));
 
-    texX = static_cast<int>(wallX * static_cast<double>(texWidth));
-    if (side == 0 && rayDirX > 0) texX = texWidth - texX - 1;
-    if (side == 1 && rayDirY < 0) texX = texWidth - texX - 1;
-    step = 1.0 * texHeight / lineHeight;
-    texPos = (drawStart - screenHeight / 2 + lineHeight / 2) * step;
+    texX = static_cast<int>(wallX * static_cast<double>(TEX_WIDTH));
+    if (side == 0 && rayDir.x > 0) texX = TEX_WIDTH - texX - 1;
+    if (side == 1 && rayDir.y < 0) texX = TEX_WIDTH - texX - 1;
+    step = 1.0 * TEX_HEIGHT / lineHeight;
+    texPos = (drawStart - RENDER_HEIGHT / 2 + lineHeight / 2) * step;
 }
 
 void App::drawColumn(const int x, const int side, const int drawStart, const int drawEnd, const int texNum,
                      const int texX, const double step, double texPos) const {
     for (int y = drawStart; y < drawEnd; ++y) {
         // Cast the texture coordinate to integer, and mask with (texHeight - 1) in case of overflow
-        const int texY = static_cast<int>(texPos) & (texHeight - 1);
+        const int texY = static_cast<int>(texPos) & (TEX_HEIGHT - 1);
         texPos += step;
         auto color = textures[texNum]->getPixel(texX, texY);
         // make color darker for y-sides: R, G and B byte each divided through two with a "shift" and an "and"
@@ -216,20 +215,20 @@ void App::drawColumn(const int x, const int side, const int drawStart, const int
 }
 
 void App::render() const {
-    for (int x = 0; x < screenWidth; x++) {
+    for (int x = 0; x < RENDER_WIDTH; x++) {
         clearBuffer(x);
 
-        double rayDirX, rayDirY;
-        int mapX, mapY;
+        sf::Vector2<double> rayDir;
+        const sf::Vector2i map;
         double perpWallDist;
         int side, lineHeight, drawStart, drawEnd;
 
-        calculate(x, rayDirX, rayDirY, mapX, mapY, perpWallDist,
+        calculate(x, rayDir, map, perpWallDist,
                   side, lineHeight, drawStart, drawEnd);
 
         int texNum, texX;
         double step, texPos;
-        getTextureParameters(rayDirX, rayDirY, mapX, mapY, perpWallDist,
+        getTextureParameters(rayDir, map, perpWallDist,
                              side, lineHeight, drawStart, texNum, texX, step, texPos);
         //draw the pixels of the stripe as a vertical line
         drawColumn(x, side, drawStart, drawEnd, texNum, texX, step, texPos);
@@ -237,6 +236,6 @@ void App::render() const {
 
     texture->update(*buffer);
     window->clear(sf::Color::Black);
-    window->draw(*bufferSprite);
+    window->draw(*bufferRect);
     window->display();
 }
--- src/App.h
+++ src/App.h
@@ -6,89 +6,92 @@
 #define APP_H
 #include <SFML/Graphics.hpp>
 
-#define mapWidth 24
-#define mapHeight 24
-#define texWidth 64
-#define texHeight 64
-#define screenWidth 640
-#define screenHeight 480
-#define wallDistance 8
+#define MAP_WIDTH 24
+#define MAP_HEIGHT 24
+#define TEX_WIDTH 128
+#define TEX_HEIGHT 128
+#define WINDOW_WIDTH 1920
+#define WINDOW_HEIGHT 1080
+#define RENDER_WIDTH 640
+#define RENDER_HEIGHT 360
+#define WALL_DISTANCE 8
 
-inline int worldMap[mapWidth][mapHeight]=
+inline int worldMap[MAP_WIDTH][MAP_HEIGHT] =
 {
-    {4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,7,7,7,7,7,7,7,7},
-    {4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,0,0,0,0,0,0,7},
-    {4,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7},
-    {4,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7},
-    {4,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,7,0,0,0,0,0,0,7},
-    {4,0,4,0,0,0,0,5,5,5,5,5,5,5,5,5,7,7,0,7,7,7,7,7},
-    {4,0,5,0,0,0,0,5,0,5,0,5,0,5,0,5,7,0,0,0,7,7,7,1},
-    {4,0,6,0,0,0,0,5,0,0,0,0,0,0,0,5,7,0,0,0,0,0,0,8},
-    {4,0,7,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,7,7,7,1},
-    {4,0,8,0,0,0,0,5,0,0,0,0,0,0,0,5,7,0,0,0,0,0,0,8},
-    {4,0,0,0,0,0,0,5,0,0,0,0,0,0,0,5,7,0,0,0,7,7,7,1},
-    {4,0,0,0,0,0,0,5,5,5,5,0,5,5,5,5,7,7,7,7,7,7,7,1},
-    {6,6,6,6,6,6,6,6,6,6,6,0,6,6,6,6,6,6,6,6,6,6,6,6},
-    {8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,4},
-    {6,6,6,6,6,6,0,6,6,6,6,0,6,6,6,6,6,6,6,6,6,6,6,6},
-    {4,4,4,4,4,4,0,4,4,4,6,0,6,2,2,2,2,2,2,2,3,3,3,3},
-    {4,0,0,0,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,0,0,0,2},
-    {4,0,0,0,0,0,0,0,0,0,0,0,6,2,0,0,5,0,0,2,0,0,0,2},
-    {4,0,0,0,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,2,0,2,2},
-    {4,0,6,0,6,0,0,0,0,4,6,0,0,0,0,0,5,0,0,0,0,0,0,2},
-    {4,0,0,5,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,2,0,2,2},
-    {4,0,6,0,6,0,0,0,0,4,6,0,6,2,0,0,5,0,0,2,0,0,0,2},
-    {4,0,0,0,0,0,0,0,0,4,6,0,6,2,0,0,0,0,0,2,0,0,0,2},
-    {4,4,4,4,4,4,4,4,4,4,1,1,1,2,2,2,2,2,2,3,3,3,3,3}
+    {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 7, 7, 7, 7, 7, 7, 7, 7},
+    {4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 7},
+    {4, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7},
+    {4, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7},
+    {4, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 7},
+    {4, 0, 4, 0, 0, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7, 7, 0, 7, 7, 7, 7, 7},
+    {4, 0, 5, 0, 0, 0, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 7, 0, 0, 0, 7, 7, 7, 1},
+    {4, 0, 6, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 0, 0, 0, 8},
+    {4, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 1},
+    {4, 0, 8, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 0, 0, 0, 8},
+    {4, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 7, 7, 7, 1},
+    {4, 0, 0, 0, 0, 0, 0, 5, 5, 5, 5, 0, 5, 5, 5, 5, 7, 7, 7, 7, 7, 7, 7, 1},
+    {6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+    {8, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+    {6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6},
+    {4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 6, 0, 6, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3},
+    {4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2},
+    {4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 2, 0, 0, 5, 0, 0, 2, 0, 0, 0, 2},
+    {4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2},
+    {4, 0, 6, 0, 6, 0, 0, 0, 0, 4, 6, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 2},
+    {4, 0, 0, 5, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2},
+    {4, 0, 6, 0, 6, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 5, 0, 0, 2, 0, 0, 0, 2},
+    {4, 0, 0, 0, 0, 0, 0, 0, 0, 4, 6, 0, 6, 2, 0, 0, 0, 0, 0, 2, 0, 0, 0, 2},
+    {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3}
 };
 
 class App {
 public:
     int run();
+
     App();
+
     ~App();
 
 private:
-    sf::RenderWindow* window;
+    sf::RenderWindow *window;
     sf::Clock deltaClock;
-    sf::Image* buffer;
-    sf::Texture* texture;
-    sf::Sprite* bufferSprite;
+    sf::Image *buffer;
+    sf::Texture *texture;
+    sf::RectangleShape *bufferRect;
 
-    std::vector<sf::Image*> textures;
+    std::vector<sf::Image *> textures;
 
     double posX = 22;
     double posY = 11.5;
     double dirX = -1;
     double dirY = 0;
     double planeX = 0;
-    double planeY = 0.66;
+    double planeY = 0.9;
 
 private:
     void handleInput();
 
     void clearBuffer(int x) const;
 
-    void calculateStep(const double &rayDirX, const double &rayDirY, const int &mapX, const int &mapY,
-                       double &sideDistX, double &sideDistY, double deltaDistX, double deltaDistY,
-                       int &stepX, int &stepY) const;
+    auto calculateStep(const sf::Vector2<double> &rayDir, const sf::Vector2<double> &deltaDist,
+                       sf::Vector2i map, sf::Vector2<double> &sideDist,
+                       sf::Vector2i &step) const -> void;
 
-    static void performDDA(int &mapX, int &mapY, int &side, double &sideDistX, double &sideDistY, double deltaDistX,
-                           double deltaDistY, int stepX, int stepY);
+    static auto performDDA(int &side, const sf::Vector2<double> &deltaDist, const sf::Vector2i &step, sf::Vector2i map,
+                           sf::Vector2<double> &sideDist) -> void;
 
-    void calculate(int x, double &rayDirX, double &rayDirY, int &mapX, int &mapY, double &perpWallDist,
-                   int &side, int &lineHeight, int &drawStart, int &drawEnd) const;
+    auto calculate(int x, sf::Vector2<double> &rayDir, sf::Vector2i map, double &perpWallDist,
+                   int &side, int &lineHeight, int &drawStart, int &drawEnd) const -> void;
 
     void drawColumn(int x, int side, int drawStart, int drawEnd, int texNum, int texX, double step,
                     double texPos) const;
 
-    void getTextureParameters(double rayDirX, double rayDirY, int mapX, int mapY, double perpWallDist, int side,
+    auto getTextureParameters(const sf::Vector2<double> &rayDir, sf::Vector2i map, double perpWallDist, int side,
                               int lineHeight, int drawStart, int &texNum, int &texX, double &step,
-                              double &texPos) const;
+                              double &texPos) const -> void;
 
     void render() const;
 };
-
 
 
 #endif //APP_H
